40a0f0c0735397db8f435ad8bcded16b3c1cefbd
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-30 02:01:49 -0400

    add shared option change source method append BOOST_ALL_NO_LIB

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,13 @@
 from conans import ConanFile, tools, os
 
+
 class BoostGraphConan(ConanFile):
     name = "Boost.Graph"
     version = "1.64.0"
-    generators = "boost"
     url = "https://github.com/bincrafters/conan-boost-graph"
-    source_url = "https://github.com/boostorg/graph"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     requires =  "Boost.Level14Group/1.64.0@bincrafters/testing"
-   
-    def package_id(self):
-        self.info.header_only()
         
     #This library is part of one or more cyclic dependency groups within Boost.
     
