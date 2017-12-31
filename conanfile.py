077b0bcd852b1446589ddabaaafb128058197954
Jerry W <jerrywiltse@gmail.com>
2017-12-31 18:18:46 -0500

    Update url field in conanfile [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ from conans import ConanFile, tools
 class BoostGraphConan(ConanFile):
     name = "boost_graph"
     version = "1.66.0"
-    url = "https://github.com/bincrafters/conan-boost-graph"
+    url = "https://github.com/bincrafters/conan-boost_graph"
 
     lib_short_names = ["graph"]
     is_in_cycle_group = True
