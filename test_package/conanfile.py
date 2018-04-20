8a88323729a8bef732b5021cd99df810b4661326
Rene Rivera <grafikrobot@gmail.com>
2018-04-20 15:50:32 -0500

    Templated test_package [skip ci]

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -9,14 +9,19 @@ class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
+    def requirements(self):
+        boost_deps = ['algorithm', 'any', 'array', 'assert', 'bimap', 'bind', 'concept_check', 'config', 'container_hash', 'conversion', 'core', 'detail', 'disjoint_sets', 'foreach', 'function', 'graph', 'graph_parallel', 'integer', 'iterator', 'lexical_cast', 'math', 'move', 'mpl', 'multi_index', 'optional', 'parameter', 'preprocessor', 'property_map', 'property_tree', 'random', 'range', 'serialization', 'smart_ptr', 'spirit', 'static_assert', 'test', 'throw_exception', 'tti', 'tuple', 'type_traits', 'typeof', 'unordered', 'utility', 'xpressive']
+        for lib in boost_deps:
+            self.requires("boost_" + lib + "/1.67.0@" + self.user + "/" + self.channel)
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
-        
+
     def test(self):
         with tools.environment_append(RunEnvironment(self).vars):
             if self.settings.os == "Windows":
-                self.run(os.path.join("bin","test_package"))
+                self.run(os.path.join("bin", "test_package"))
             else:
-                self.run("DYLD_LIBRARY_PATH=%s %s"%(os.environ['DYLD_LIBRARY_PATH'],os.path.join("bin","test_package")))
+                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ['DYLD_LIBRARY_PATH'], os.path.join("bin", "test_package")))
